b5cbd36df37b427918c9d28466f48fff284d2fda
unknown <edhar97@gmail.com>
2023-11-25 00:11:59 +0300

    decided kata

---
+++ 8kyu/Can we divide it.js
@@ -0,0 +1,3 @@
+// Your task is to create the functionisDivideBy (or is_divide_by) 
+// to check if an integer number is divisible by both integers a and b.
+let isDivideBy = (number, a, b) => number % a === 0 && number % b === 0;
