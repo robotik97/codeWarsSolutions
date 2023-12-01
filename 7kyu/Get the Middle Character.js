6a38483456c63ecaac63ffd58728ae5f2115c214
Edgar Krasikov <edhar97@gmail.com>
2023-12-01 22:44:17 +0300

    Update Get the Middle Character.js

--- 7kyu/Get the Middle Character.js
+++ 7kyu/Get the Middle Character.js
@@ -1,13 +1,13 @@
-Kata.getMiddle("middle") should return "dd"
+//Kata.getMiddle("middle") should return "dd"
 
-Kata.getMiddle("A") should return "A"
-#Input
+//Kata.getMiddle("A") should return "A"
+//#Input
 
-A word (string) of length 0 < str < 1000 (In javascript you may get slightly more than 1000 in some test cases due to an error in the test cases). You do not need to test for this. This is only here to tell you that you do not need to worry about your solution timing out.
+//A word (string) of length 0 < str < 1000 (In javascript you may get slightly more than 1000 in some test cases due to an error in the test cases). You do not need to test for this. This is only here to tell you that you do not need to worry about your solution timing out.
 
-#Output
+//#Output
 
-The middle character(s) of the word represented as a string.
+//The middle character(s) of the word represented as a string.
 
 function getMiddle(s) {
   var middle = s.length / 2;
