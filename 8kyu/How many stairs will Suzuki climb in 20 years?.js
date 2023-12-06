9218aa4fba78532d888ce5e8c85407a76dd6175d
Edgar Krasikov <edhar97@gmail.com>
2023-12-06 22:01:16 +0300

    Update How many stairs will Suzuki climb in 20 years?.js

--- 8kyu/How many stairs will Suzuki climb in 20 years?.js
+++ 8kyu/How many stairs will Suzuki climb in 20 years?.js
@@ -7,7 +7,7 @@
 //You will receive the following data structure representing the stairs Suzuki logged in a year. You will have all data for the entire year so regardless of how it is logged the problem should be simple to solve.
 
 //stairs = [sunday,monday,tuesday,wednesday,thursday,friday,saturday]
-Make sure your solution takes into account all of the nesting within the stairs array.
+//Make sure your solution takes into account all of the nesting within the stairs array.
 
 //Each weekday in the stairs array is an array.
 
