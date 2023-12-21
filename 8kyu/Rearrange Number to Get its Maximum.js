37ad07bdb94af714ed8a0a014d164ee639e3fe16
Edgar Krasikov <edhar97@gmail.com>
2023-12-21 22:51:33 +0300

    decided kata

---
+++ 8kyu/Rearrange Number to Get its Maximum.js
@@ -0,0 +1,14 @@
+// Create a function that takes one positive three digit integer and rearranges its digits to get the maximum possible number. Assume that the argument is an integer.
+
+// Return -1 if the argument is not valid
+// Return null (nil for Ruby, nothing for Julia) if the argument is not valid.
+
+// maxRedigit(123); // returns 321
+
+let maxRedigit = function(num) {
+  if ( num<=0 || String(num).length !==3 ) {
+    return null
+ } 
+  let a = String(num)
+      return Number([...a].sort((a, b) => Number(b) - Number(a)).join(''))
+  }
