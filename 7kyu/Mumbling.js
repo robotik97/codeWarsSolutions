e89c7c631afda8c7053615daa2f82cd3a19aeb16
Edgar Krasikov <edhar97@gmail.com>
2023-12-25 21:53:07 +0300

    decided kata

---
+++ 7kyu/Mumbling.js
@@ -0,0 +1,10 @@
+// This time no story, no theory. The examples below show you how to write function accum:
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+function accum(s) {
+  return [...s].map((letters, index) => 
+  (letters.toUpperCase() + letters.toLowerCase().repeat(index))).join("-");
+}
