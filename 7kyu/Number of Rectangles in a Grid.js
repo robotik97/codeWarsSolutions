e5a9b1cbc5779a4f7848851faa23d86283b79441
Edgar Krasikov <edhar97@gmail.com>
2024-02-02 21:29:58 +0300

    add second solution.js

--- 7kyu/Number of Rectangles in a Grid.js
+++ 7kyu/Number of Rectangles in a Grid.js
@@ -1,11 +1,8 @@
 // Given a grid of size m x n, calculate the total number of rectangles contained in this rectangle. All integer sizes and positions are counted.
-
 // Examples(Input1, Input2 --> Output):
-
 // 3, 2 --> 18
 // 4, 4 --> 100
 // Here is how the 3x2 grid works (Thanks to GiacomoSorbi for the idea):
-
 // 1 rectangle of size 3x2:
 
 // [][][]
@@ -31,6 +28,17 @@
 
 // There is a very simple solution to this!
 
+// first solution 
+function numberOfRectangles(m, n) {
+  let res = 0
+  for (i=1; i <=m; i++) {
+    for (j=1; j<=n; j++) {
+      res += i * j
+    }
+  } return res
+}
+
+// second solution
 function numberOfRectangles(m, n) {
   return (((m**2) + m)*((n**2) + n))/4
 }
