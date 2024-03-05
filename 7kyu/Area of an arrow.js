640e30d34e2958ddc7e6c41d382a9ffd8733e005
Edgar Krasikov <edhar97@gmail.com>
2024-03-05 22:43:51 +0300

    decided kata

---
+++ 7kyu/Area of an arrow.js
@@ -0,0 +1,10 @@
+//Area of an arrow
+//An arrow is formed in a rectangle with sides a and b by joining the bottom corners to the midpoint of the top edge and the centre of the rectangle.
+
+//a and b are integers and > 0
+
+//Write a function which returns the area of the arrow.
+
+function arrowArea(a,b) {
+  return (a*b/4).toFixed(2)*1
+}
