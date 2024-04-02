27a1d07f27359a185208711a783c5b15277bd9ef
Edgar Krasikov <edhar97@gmail.com>
2024-04-02 21:49:46 +0300

    decided kata

---
+++ 7kyu/Working with arrays I (and why your code fails in some katas).js
@@ -0,0 +1,7 @@
+//In this kata the function returns an array/list of numbers without its last element. The function is already written for you and the basic tests pass, but random tests fail. Your task is to figure out why and fix it.
+
+//Good luck!
+
+function withoutMax(arr) {
+  return arr.slice(0,-1);
+}
